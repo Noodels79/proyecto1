a4b382692547f571023db31b2624e5f239385813
Mario 2 <irubai2003@gmail.com>
2021-02-13 18:33:57 +0100

    version2

---
+++ proyecto1/src/proyecto1/Principal.java
@@ -0,0 +1,11 @@
+package proyecto1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo java-git");
+		System.out.println("cambio 1 echo principal");
+	}
+
+}
