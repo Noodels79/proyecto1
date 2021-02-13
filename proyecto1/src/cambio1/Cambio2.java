0883090768b8d1f6366d5a0ba2824d02a22c4169
Mario 2 <irubai2003@gmail.com>
2021-02-13 20:25:24 +0100

    cambio proyecto2

---
+++ proyecto1/src/cambio1/Cambio2.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Case cambio2 en proyecto2");
+		System.out.println("Case cambio2 en proyecto2");
+	}
+
+}
